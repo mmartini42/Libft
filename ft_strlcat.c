978c92971d9ad2a2177b931f67c5c2fdb810e0f0
mathis <mathismartini1@gmail.com>
2021-09-27 21:53:28 +0200

    strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matmarti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/27 20:39:26 by matmarti          #+#    #+#             */
+/*   Updated: 2021/09/27 21:52:29 by matmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (dst[i] && i < dstsize)
+		i++;
+	j = 0;
+	if (i < dstsize)
+	{
+		while (src[j] && i < dstsize - 1)
+		{
+			dst[i] = src[j];
+			i++;
+			j++;
+		}
+		dst[i] = '\0';
+	}
+	while (src[j])
+	{
+		i++;
+		j++;
+	}
+	return (i);
+}
