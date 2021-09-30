555d34b5d20e87f83c83ad6d2480f56eb54fd913
mathis <mathismartini1@gmail.com>
2021-09-30 21:48:32 +0200

    Alloue de la memoire

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: matmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 19:01:33 by matmarti          #+#    #+#             */
-/*   Updated: 2021/09/30 21:33:08 by matmarti         ###   ########.fr       */
+/*   Updated: 2021/09/30 21:38:57 by matmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,6 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+void	*ft_memalloc(size_t size);
 
 #endif
