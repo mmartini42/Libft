65722461bf6edfca337adb36d88f715aa9e741ec
mathis <mathismartini1@gmail.com>
2021-09-30 14:01:11 +0200

    strrchr

---
+++ ft_strrchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matmarti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/29 19:50:27 by matmarti          #+#    #+#             */
+/*   Updated: 2021/09/30 13:49:43 by matmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	i;
+	char	*last;
+
+	last = NULL;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			last = (char *)&s[i];
+		i++;
+	}
+	if ((char)c == s[i])
+		return ((char *)&s[i]);
+	return (last);
+}
