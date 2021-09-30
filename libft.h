6ac51f2d4db2a493fc44d7ee29f159825934fd71
mathis <mathismartini1@gmail.com>
2021-09-30 22:36:33 +0200

    applique un pointeur sur fonction et stoque le resultat dans une nouvelle chaine

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: matmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 19:01:33 by matmarti          #+#    #+#             */
-/*   Updated: 2021/09/30 22:14:21 by matmarti         ###   ########.fr       */
+/*   Updated: 2021/09/30 22:23:52 by matmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,5 +52,6 @@ void	ft_strdel(char **as);
 void	ft_strclr(char *s);
 void	ft_striter(char *s, void (*f)(char *));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strmap(char const *s, char (*f)(char));
 
 #endif
