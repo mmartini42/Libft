8f54506f94049455266f2b86027d6a6aad608ca6
mathis <mathismartini1@gmail.com>
2021-09-30 14:13:08 +0200

    strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matmarti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/30 14:02:38 by matmarti          #+#    #+#             */
+/*   Updated: 2021/09/30 14:12:42 by matmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *str, const char *to_find, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (to_find == NULL || *to_find == '\0')
+		return ((char *)str);
+	i = 0;
+	while (str[i])
+	{
+		j = 0;
+		while (to_find[j] == str[i + j] && i + j < len)
+		{
+			if (to_find[j + 1] == '\0')
+				return ((char *)str + i);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
