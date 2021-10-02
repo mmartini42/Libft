c9a398f3fb112b0b63150bfba815d8d5d04d53a1
mathis <mathismartini1@gmail.com>
2021-10-02 23:52:13 +0200

    Renvoie le dernier element de la liste chaine

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: matmarti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 19:01:33 by matmarti          #+#    #+#             */
-/*   Updated: 2021/10/02 23:44:15 by matmarti         ###   ########.fr       */
+/*   Updated: 2021/10/02 23:50:33 by matmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,5 +78,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **alst, t_list *node);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
